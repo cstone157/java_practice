139e5f6990f3010bb5326510f42066b6b91e122d
Culver Stone <culver.stone57@gmail.com>
2024-12-18 20:34:21 -0800

    Fixed the extended euclid

--- art_of_computer_programming/book01/ch01/02/ExtEuclid.java
+++ art_of_computer_programming/book01/ch01/02/ExtEuclid.java
@@ -16,36 +16,20 @@ class ExtEuclid{
 
         //int m = rand.nextInt(10000);
         //int n = rand.nextInt(10000);
-        int m = 1769;
-        int n = 551;
+        int m_org = 1769;
+        int n_org = 551;
+        int m = m_org;
+        int n = n_org;
 
         int[] a = {0, 1};
         int[] b = {1, 0};
         int c = m;
         int d = n;
         
-        // if m is less than n swap them
-        if (m < n) {
-            int tmp = m;
-            m = n;
-            n = tmp;
-        }
+        int r = m;
+        int q = 0;
 
-        int r = m % n;
-        int q = m / n;
-        
-        // while r is greater than 0
         while (r > 0) {
-            System.out.println("a' = " + a[1] + "\ta = " + a[0] + "\tb' = " + b[1] + "\tb = " + b[0] + "\tc = " + c + "\td = " + d + "\tq = " + q + "\tr = " + r);
-            c = d;
-            d = r;
-            int t = a[1];
-            a[1] = a[0];
-            a[0] = t - q*a[0];
-            t = b[1];
-            b[1] = b[0];
-            b[0] = t - 1*b[0];
-
             if (m < n) {
                 int tmp = m;
                 m = n;
@@ -53,18 +37,31 @@ class ExtEuclid{
             }
 
             r = m % n;
-
+            q = m / n;
             if (r == 0)
                 break;
-
+            
             m = n;
             n = r;
+
+            System.out.println("a' = " + a[1] + "\ta = " + a[0] + "\tb' = " + b[1] + "\tb = " + b[0] + "\tc = " + c + "\td = " + d + "\tq = " + q + "\tr = " + r);
+
+            
+            c = d;
+            d = r;
+            int t = a[1];
+            a[1] = a[0];
+            a[0] = t - q * a[0];
+            t = b[1];
+            b[1] = b[0];
+            b[0] = t - q * b[0];
         }
 
         System.out.println(" ============================================ ");
         System.out.println("a' = " + a[1] + "\ta = " + a[0] + "\tb' = " + b[1] + "\tb = " + b[0] + "\tc = " + c + "\td = " + d + "\tq = " + q + "\tr = " + r);
-        System.out.println("Greatest Common Divisor = " + d);
-        System.out.println("m = " + m);
-        System.out.println("n = " + n);
+        System.out.println("a{" + a[0] + "} * m{" + m + "} + b{" + b[0] + "} * n{" + n + "} = d{" + d + "}");
+        //System.out.println("Greatest Common Divisor = " + d);
+        //System.out.println("m = " + m);
+        //System.out.println("n = " + n);
     }  
 }
